f851c8c9f72a79386688ff095404c09bf797b979
Kartik Varshney <kartikvarshney30@gmail.com>
2026-02-21 12:00:01 +0530

    Kartik .Added Quantity Measurement App 1st UC

---
+++ QuantityMeasurementApp/QuantityMeasurementAppMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using QuantityMeasurementApp.Models;
+
+namespace QuantityMeasurementApp;
+
+public class QuantityMeasurementAppMain
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter first value in feet: ");
+            double feetValue1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second value in feet: ");
+            double feetValue2 = Convert.ToDouble(Console.ReadLine());
+
+            Feet firstFeet = new Feet(feetValue1);
+            Feet secondFeet = new Feet(feetValue2);
+
+            bool result = firstFeet.Equals(secondFeet);
+            Console.WriteLine(result ? "Equals (true)" : "Not Equal (false)");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid Value! Enter Numeric Values Only");
+        }
+    }
+}
